dc5c3b082f925e91f5b67255f62b231e9d5a091b
George645 <gsvbutterfly@outlook.com>
2025-03-31 00:10:48 +0100

    getting the board working

--- Assets/Scripting/Square Colour Correction Script.cs
+++ Assets/Scripting/Square Colour Correction Script.cs
@@ -6,13 +6,26 @@ public class SquareColourCorrection : MonoBehaviour{
     List<Material> temporaryList = new();
     public static Material blackMaterial;
     public static Material whiteMaterial;
+    public int thisNumber = 0;
+    public static GameObject cube;
+    public GameObject cubetemp;
 
     void Awake(){
         if (blackMaterial == null) {
             blackMaterial = temporaryList[0];
             whiteMaterial = temporaryList[1];
+            cube = cubetemp;
         }
-        AlignToGrid();
+        AlignToGridAndColour();
+    }
+    private void Start() {
+        //Debug.Log(GetSquareInDirection(-1, 0));
+        if (thisNumber == 0) {
+            CreateEightByEightBoard();
+        }
+        if (thisNumber > 0 && thisNumber < ) {
+            SpawnSurrounding();
+        }
     }
     void Update(){
         if (GetSquareInDirection(-1, -1) == null && GetSquareInDirection(-1, 0) == null && GetSquareInDirection(-1, 1) == null && GetSquareInDirection(0, 1) == null && GetSquareInDirection(1, 1) == null && GetSquareInDirection(1, 0) == null && GetSquareInDirection(1, -1) == null && GetSquareInDirection(0, -1)) {
@@ -30,9 +43,51 @@ public class SquareColourCorrection : MonoBehaviour{
         }
     }
 
+    public void SpawnSurrounding() {
+        GameObject currentCube;
+        if (GetSquareInDirection(-1, -1) == null) {
+            //Debug.Log(this);
+            //Debug.Log(GetSquareInDirection(-1, -1));
+            currentCube = Instantiate(cube, new Vector3(transform.position.x - 1, 0, transform.position.z - 1), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(-1, 0) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x - 1, 0, transform.position.z), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(-1, 1) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x - 1, 0, transform.position.z + 1), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(0, -1) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x, 0, transform.position.z - 1), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(0, 1) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x, 0, transform.position.z + 1), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(1, -1) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x + 1, 0, transform.position.z - 1), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(1, 0) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x + 1, 0, transform.position.z), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+        if (GetSquareInDirection(1, 1) == null) {
+            currentCube = Instantiate(cube, new Vector3(transform.position.x + 1, 0, transform.position.z + 1), Quaternion.identity);
+            currentCube.GetComponent<SquareColourCorrection>().thisNumber = thisNumber + 1;
+        }
+    }
+
+    void CreateEightByEightBoard() {
+        SpawnSurrounding();
+    }
+
     GameObject GetSquareInDirection(float x, float y) {
         GameObject returningGameObject = null;
-        Ray newRay = new(new Vector3(transform.position.x + x, 0, transform.position.z), new Vector3(0, 1, 0));
+        Ray newRay = new(new Vector3(transform.position.x + x, -0.2f, transform.position.z + y), new Vector3(0, 100, 0));
         if (Physics.Raycast(newRay, out RaycastHit hitInfo)) {
             returningGameObject = hitInfo.collider.gameObject;
         }
