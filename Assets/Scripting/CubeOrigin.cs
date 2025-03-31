a59d5b6ca53e2fdcbcaa5a6315084e6a2ab0fa6f
George645 <gsvbutterfly@outlook.com>
2025-03-31 15:57:40 +0100

    remove squares exists and looks good
    Remove squares slowly moves in from the corners to remove squares

--- Assets/Scripting/CubeOrigin.cs
+++ Assets/Scripting/CubeOrigin.cs
@@ -4,11 +4,11 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class SquareColourCorrection : CubeBase {
-    private int thisNumber = 0;
     public static GameObject cube;
     public GameObject cubetemp;
     public int sizeNumber;
     private bool running;
+    int maxValue;
 
     void Awake(){
         if (blackMaterial == null) {
@@ -20,18 +20,18 @@ public class SquareColourCorrection : CubeBase {
     }
     private void Start() {
         //Debug.Log(GetSquareInDirection(-1, 0));
-        if (thisNumber == 0) {
-            CreateEightByEightBoard();
-        }
         StartCoroutine(SpawnSurrounding());
     }
     void Update(){
+
     }
 
     public void setSizeOfBoard(Slider slider) {
         sizeNumber = (int)slider.value/* * 2 + 1*/;
+        maxValue = (int)slider.maxValue;
         if (false == running) {
             StartCoroutine(SpawnSurrounding());
+            StartCoroutine(RemoveSurrounding());
         }
     }
 
@@ -58,6 +58,29 @@ public class SquareColourCorrection : CubeBase {
             }
         }
         running = false;
+        yield return null;
+    }
+    IEnumerator RemoveSurrounding() {
+        for (int i = maxValue * 2; i>=0; i--) {
+            for (int x = maxValue; x >= 0; x--) {
+                int z = i - x;
+                if (z > maxValue) {
+                    break;
+                }
+                
+                if(Mathf.Abs(z) <= sizeNumber && Mathf.Abs(x) <= sizeNumber || (GetSquareInDirection(z, x) && GetSquareInDirection(z, -x) && GetSquareInDirection(-z, x) && GetSquareInDirection(-z, -x)) == null) {
+                    continue;
+                }
+
+                
+                Debug.Log(x + ", " + z + ", " + GetSquareInDirection(x, z));
+                Destroy(GetSquareInDirection(z, x));
+                Destroy(GetSquareInDirection(z, -x));
+                Destroy(GetSquareInDirection(-z, x));
+                Destroy(GetSquareInDirection(-z, -x));
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
         yield return null;
     }
 
