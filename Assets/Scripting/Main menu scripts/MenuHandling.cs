d137ede9a40366c88f13647e51c96ab05a6c7e23
George645 <gsvbutterfly@outlook.com>
2025-04-29 17:21:21 +0100

    lerp camera in main menu

--- Assets/Scripting/Main menu scripts/MenuHandling.cs
+++ Assets/Scripting/Main menu scripts/MenuHandling.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -17,12 +18,26 @@ public class MenuHandling : MonoBehaviour{
     }
     public void SwapMenu() {
         try {
-            alternateMenuToSwapTo.SetActive(true);
-            transform.parent.gameObject.SetActive(false);
+            StartCoroutine(MoveCamera(transform.position));
         }
         catch {
             Debug.LogError("FIX THIS YOU IDIOT");
         }
+    }
+    IEnumerator MoveCamera(Vector3 position) {
+        for (float i = 1; i <= 25; i++) {
+            Debug.Log(i / 10);
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, position, 0.001f);
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return new WaitForSeconds(0.01f);
+        alternateMenuToSwapTo.SetActive(true);
+
+        yield return null;
+    }
+    public void Return() {
+        alternateMenuToSwapTo.SetActive(false);
+        StartCoroutine(MoveCamera(new Vector3(0.5f, 8, -0.5f)));
     }
     public void adjustSensitivity() {
         int input = (int)transform.GetComponent<Slider>().value;
