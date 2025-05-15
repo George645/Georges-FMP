9e48fbc96ba59cdc32d3dfec6b03aa992afacce1
George645 <gsvbutterfly@outlook.com>
2025-05-15 16:17:23 +0100

    This damn AI

--- Assets/Scripting/AI/AI.cs
+++ Assets/Scripting/AI/AI.cs
@@ -1,36 +1,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using UnityEngine.Animations;
 using System;
+using UnityEditor;
+using UnityEngine.UIElements;
 
 public class AI : MonoBehaviour {
-
-    #region Constants
-    #endregion
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static AI ai;
     public List<PieceMovement> PlayersTeam;
     public List<PieceMovement> AITeam;
+    bool isEnemysTurn = false;
+    bool firstSplitSecondCheck = true;
 
     void Awake() {
         ai = this;
     }
+    private void Start() {
+        
+    }
 
-    int Evaluation() {
+    int Evaluation(List<PieceMovement> localPlayersTeam, List<PieceMovement> localAITeam) {
         int evaluation = 0;
-        foreach (PieceMovement piece in PlayersTeam) {
+        foreach (PieceMovement piece in localPlayersTeam) {
             //WORK ON THIS
             evaluation -= GetAmountOfMaterial(piece);
         }
-        foreach (PieceMovement piece in AITeam) {
+        foreach (PieceMovement piece in localAITeam) {
             evaluation += GetAmountOfMaterial(piece);
         }
         return evaluation;
     }
 
-    //Write down what sort of systems you want, what you want them to do you for and so on
+    (int, int, int) Search(int depth, int startingDepth, List<PieceMovement> localPlayersTeam, List<PieceMovement> localAITeam) {
+        Vector2Int position = Vector2Int.zero;
+        int evaluation = int.MinValue;
+        depth -= 1;
+        if (depth != 0) {
+            if ((startingDepth - depth) / 3 % 2 == 0) {
+                foreach (PieceMovement movement in localAITeam) {
+                    for (int j = 0; j < movement.moveableTiles.Length; j++) {
+                        bool[] array = movement.moveableTiles[j];
+                        for (int k = 0; k < array.Length; k++) {
+                            bool boolean = array[k];
+                            if (boolean == false){
+                                continue;
+                            }
+                            if (movement.infinitelyScalingRange) {
+                                for (int l = 0; l < k; l++) {
+                                    if ((l != k || movement.thisObject.PieceInDirection(j - movement.currentRange - 1, k - movement.currentRange - 1).GetComponent<UnderlyingPiece>().playersTeam) && movement.thisObject.PieceInDirection(j - movement.currentRange - 1, k - movement.currentRange - 1) != null) {
+                                        continue;
+                                    }
+                                }
+                            }
+                                List<PieceMovement> inputList = localPlayersTeam;
+                            if (movement.thisObject.PieceInDirection(j - movement.currentRange - 1, k - movement.currentRange - 1) != null) {
+                                inputList.Remove(movement.thisObject.PieceInDirection(j - movement.currentRange - 1, k - movement.currentRange - 1).GetComponent<UnderlyingPiece>().thisPiece);
+                            }
+                            var thisSearch = Search(depth, startingDepth, inputList, localAITeam);
+                            if (thisSearch.Item1 > evaluation) {
+                                evaluation = thisSearch.Item1;
+                            }
+                        }
+
+                    }
+                }
+            }
+            else if ((startingDepth - depth) / 3 % 2 == 1) {
+
+            }
+        }
+        return (evaluation, position.x, position.y);
+    }
+
+    void FigureOutANameAndAReturnType() {
+
+    }
 
     int GetAmountOfMaterial(PieceMovement movement) {
         return movement.name switch {
@@ -48,6 +93,17 @@ public class AI : MonoBehaviour {
             "Pedestal" => throw new NotImplementedException(),
             _ => throw new KeyNotFoundException("name not found: " + movement.name),
         };
+    }
+
+    float StartTime;
+    private void Update() {
+        if (isEnemysTurn) {
+            if (firstSplitSecondCheck) {
+                StartTime = Time.time;
+                firstSplitSecondCheck = !firstSplitSecondCheck;
+            }
+            isEnemysTurn = Time.time - StartTime <= 1;
+        }
     }
 
     public void BeginTurn() {
