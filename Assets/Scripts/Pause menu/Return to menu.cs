76c21088fabab0bc07c5d4fe58eecc306f406b9a
George645 <gsvbutterfly@outlook.com>
2025-06-09 10:23:02 +0100

    Copied in scripts from other project - added camera movement

--- Assets/Scripts/Pause menu/Return to menu.cs
+++ Assets/Scripts/Pause menu/Return to menu.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 namespace TC_PauseMenu {
     public class Returntomenu : MonoBehaviour {
         public void ReturnToMenu() {
-            TC_Menu.MenuHandling.boardState = new TC.Gamestate(TC.AI.ai.PlayersTeam, TC.AI.ai.AITeam);
             SceneManager.LoadScene("main menu");
         }
     }
